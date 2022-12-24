e9b7fadef8078c543400668ae922b6f2bdecfd71
mehran <104734004+Mehran-Jalali@users.noreply.github.com>
2022-12-24 22:47:11 +0330

    customizing ValidatinError with function for instance

--- index.js
+++ index.js
@@ -120,10 +120,20 @@ app.delete(
   })
 );
 
+const handleValidationErr = (err) => {
+  console.dir(err);
+  return new AppError(`Validation Failed...${err.message}`, 400);
+};
+
+app.use((err, req, res, next) => {
+  console.log(err.name);
+  if (err.name === "ValidationError") err = handleValidationErr(err);
+  next(err);
+});
+
 app.use((err, req, res, next) => {
   const { status = 500, message = `Something went wrong` } = err;
   res.status(status).send(message);
-  res.send(message);
 });
 
 app.listen(3000, () => {
